e4eb400e347e02e28cc85a818d216dfd2269afe6
Pratyush Madhukar <pratyush.madhukar@gmail.com>
2017-11-12 00:48:59 +0530

    db connections

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var config = {};
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e.: http://localhost:3000/3Ys
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'https://dry-dusk-18938.herokuapp.com/';
 
 // your MongoDB host and database name
-config.db.host = 'localhost';
+config.db.host = 'guest:guest@ds255265.mlab.com:55265/';
 config.db.name = 'tiny_url';
 
 module.exports = config;
